524b1abb5efc6c9c4ca0ca32e6d218626a11868c
Guilherme Pacheco <guipacheco2@gmail.com>
2021-03-04 10:24:03 -0300

    fix: react-imask does not have types

--- src/components/forms/MaskedField.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { IMaskMixin } from 'react-imask'
-import { TextField } from './TextField'
-
-export const MaskedField = IMaskMixin(({ inputRef, ...props }: any) => {
-  const Component = TextField as any
-
-  return <Component {...props} innerRef={inputRef} />
-})
